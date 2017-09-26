0fb2b3dba8b3277d6c175037e17881c7ba8af10b
yuka <yuka_sugiyama@spicule.jp>
2017-09-26 20:51:48 +0900

    フィールドにid,ageを追加

--- src/model/User.java
+++ src/model/User.java
@@ -3,16 +3,44 @@ package model;
 import java.io.Serializable;
 
 public class User implements Serializable {
+
+	private String id;
 	private String name;
-	private String pass;
+	private int age;
 
-	public User() {}
-	public User(String name, String pass) {
-		this.name = name;
-		this.pass = pass;
+	public User() {
+		this.id = "";
+		this.name = "";
+		this.age = 0;
 	}
-	public String getName() { return name; }
-	public String getPass() { return pass; }
 
+	public User(String id, String name, int age) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
 }
-
