c8a65a277dd0b0cd06ae571f9ee95c4f2cb7f3db
yuka <yuka_sugiyama@spicule.jp>
2017-10-11 00:50:40 +0900

    メソッドを一つに

--- src/model/LoginDao.java
+++ src/model/LoginDao.java
@@ -1,71 +1,54 @@
 package model;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class LoginDao {
 
-	private Connection conn = null;
-	private ResultSet rs = null;
-	private PreparedStatement ps = null;
-
 	public LoginDao() throws SQLException {
-		try {
-			//JDBCドライバのロード
-			Class.forName("com.mysql.jdbc.Driver");
 
-			this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/shoppingsite","root","marujack000");
-		}
-		catch (ClassNotFoundException cnfe) {
-			cnfe.printStackTrace();
-		}
 	}
 
+
+
+
 	/**
-     * データベースから指定されたIDとパスワードを使ってユーザ情報を検索します.
+     * データベースから指定されたIDとパスワードを使いユーザ情報を検索
      *
      * @param id	ログインID
      * @param pass	パスワード
      * @return	ユーザ情報（ResultSet）
      * @throws SQLException
      */
+	public User findUserByIdAndPass(String id,String pass) throws SQLException {
 
-	public ResultSet selectUser(String id,String pass) throws SQLException {
+		//DBに接続
+		DBManager db = new DBManager();
+		Connection conn = db.getConnection();
+
 		//SQL文を生成
-		this.ps = this.conn.prepareStatement("select name, age from user where id = ? and pass = ?");
+		PreparedStatement ps = null;
+		ps = conn.prepareStatement("select * from user where id = ? and pass = ?");
 
 		//生成したSQL文の「?」の部分にIDとパスワードをセット
-		this.ps.setString(1, id);
-		this.ps.setString(2, pass);
+		ps.setString(1, id);
+		ps.setString(2, pass);
 
-		//SQLを実行
-		this.rs = this.ps.executeQuery();
+		ResultSet rs = null;
+		rs = ps.executeQuery();
 
-		return this.rs;
-	}
-
-	/**
-     * コネクションをクローズします.
-     */
-
-	public void close() {
-		try {
-			//データベースとの接続を解除する
-			if(this.conn != null) {
-				this.conn.close();
-			}
-			if(this.ps != null) {
-				this.ps.close();
-			}
-			if(this.rs != null) {
-				this.rs.close();
-			}
-		} catch(SQLException se) {
-			//データベースとの接続解除に失敗した場合
-			se.printStackTrace();
+		if (rs.next()) {
+			User user = new User(
+					rs.getString("id"),
+					rs.getString("name"),
+					rs.getString("pass"),
+					rs.getInt("age"));
+			return user;
+		} else {
+			throw new DataNotFoundException();
 		}
+
 	}
 }
