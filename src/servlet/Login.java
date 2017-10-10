e1ed884ac80e6682a2ada20bf0c3cce786dd264a
yuka <yuka_sugiyama@spicule.jp>
2017-10-11 00:54:03 +0900

    エラー時処理の追加

--- src/servlet/Login.java
+++ src/servlet/Login.java
@@ -1,8 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import model.LoginLogic;
+import model.DataNotFoundException;
+import model.LoginDao;
 import model.User;
 
 
@@ -18,28 +19,64 @@ import model.User;
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//リクエストパラメータの取得
-		request.setCharacterEncoding("UTF-8");
-		String name = request.getParameter("name");
-		String pass = request.getParameter("pass");
-
-		//Userインスタンスの生成
-		User user = new User(name,pass);
-
-		//ログイン処理
-		LoginLogic loginLogic = new LoginLogic();
-		boolean isLogin = loginLogic.execute(user);
-
-		//ログイン成功時の処理
-		if(isLogin) {
-			HttpSession session = request.getSession();
-			session.setAttribute("loginUser", user);
-		}
-
-		//ログイン結果画面にフォワード
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/loginResult.jsp");
-		dispatcher.forward(request, response);
+	public Login() {
+		super();
 	}
 
+	//index.jspへのサーブレット
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+		request.setCharacterEncoding("utf-8");
+
+		getServletContext()
+		.getRequestDispatcher("/index.jsp")
+		.forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+		request.setCharacterEncoding("UTF-8");
+
+
+				//リクエストパラメータの取得
+				String id = request.getParameter("id");
+				String pass = request.getParameter("pass");
+
+				try {
+
+					//パラメータを渡す
+					LoginDao loginDao = new LoginDao();
+					User user = loginDao.findUserByIdAndPass(id,pass);
+
+					//セッションを取得
+					//User情報をセッションスコープに保存
+					HttpSession session=request.getSession();
+					    session.setAttribute("isLogin", user);
+
+					//mainサーブレットにフォワード
+					getServletContext()
+					.getRequestDispatcher("/index.jsp")
+					.forward(request, response);
+
+				}
+				catch(SQLException e) {
+					//解決不能な問題
+					//ログイン失敗ページに飛ばす
+					getServletContext()
+					.getRequestDispatcher("/WEB-INF/jsp/loginErr.jsp")
+					.forward(request, response);
+
+				}
+				catch(DataNotFoundException e) {
+					request.setAttribute("error", "ユーザーIDかパスワードが間違っています");
+					request.getRequestDispatcher("/index.jsp").forward(request, response);
+
+				}
+
+	}
 }
+
